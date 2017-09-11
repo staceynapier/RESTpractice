3d87f80f2a9c56cc2457591d757f64bd7780d54d
staceynapier <staceynapier86@gmail.com>
2017-09-11 18:44:19 +0100

    genre now available to view

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -100,9 +100,8 @@ UI.prototype = {
 
     for(var film of films) {
       var li = document.createElement("li");
-      var genreLi = document.createElement("li");
       this.appendText(li, film.title, "Film: ");
-      this.appendText(genreLi, film.genre, "Genre: ");
+      this.appendText(li, film.genre, "Genre: ");
 
       for(var review of film.reviews){
         this.createReview(li, review);
